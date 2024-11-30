137f4e4cd8cc659925249749b4407e9b84084fa7
Rafael <168600337+rafaelcyber1@users.noreply.github.com>
2024-11-30 19:03:21 -0300

    Update install-packages.py

--- install-packages.py
+++ install-packages.py
@@ -5,10 +5,8 @@ def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
 packages = [
-    "python-whois",
     "requests",
     "beautifulsoup4",
-    "dnspython",
 ]
 
 for package in packages:
