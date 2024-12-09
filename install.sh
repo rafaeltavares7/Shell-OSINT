87aeef02dac9cdecac9dc2d8690a64e26225f759
Rafael <168600337+rafaelcyber1@users.noreply.github.com>
2024-12-09 08:09:37 -0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Instalando..."
+apt install whois
+apt install exiftool
+apt install openssl
+apt install tor
+apt install proxychains4
+mkdir analyzedownloads
+echo "Tudo Pronto ;-)"
