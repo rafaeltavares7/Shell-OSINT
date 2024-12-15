e8ef7dfba7a592b18291565c80d0fef459082b0a
Rafael <168600337+rafaelcyber1@users.noreply.github.com>
2024-12-15 12:38:14 -0300

    Update enumdns.sh

--- enumdns.sh
+++ enumdns.sh
@@ -2,9 +2,12 @@
 echo "+---------------------------+"
 echo "| instagram: @rafael_cyber1 |"
 echo "|---------------------------|"
-echo "|------enumdns.sh V.1.0-----|"
+echo "|------enumdns.sh V.1.1-----|"
 echo "+---------------------------+"
-for url in $(cat wordlists/dns.txt);
+echo "enumdns.sh <alvo> <wordlist>"
+
+wordlist=$2
+for url in $(cat $wordlist);
 # cat vai ler a wordlist que esta na pasta wordlists
 do host $url.$1 |grep "has address"
 
