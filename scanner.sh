ac87c1e9e8f2966d5ad92a756ae4c67fb69846bc
Rafael <168600337+rafaelcyber1@users.noreply.github.com>
2024-12-15 12:45:25 -0300

    Update scanner.sh

--- scanner.sh
+++ scanner.sh
@@ -4,10 +4,11 @@ echo "| instagram: @rafael_cyber1 |"
 echo "|---------------------------|"
 echo "|------scanner.sh V.1.0-----|"
 echo "+---------------------------+"
+service tor start
 # Usando xargs para rodar até 3 processos simultaneamente
 cat wordlists/top_portas.txt | xargs -P 3 -I {} bash -c '
 # Com o -I {} o xargs passa cada linha como argumento pro comando bash -c
-  nc -zv -w 2 $1 {} 2>&1 | grep -i "open" 
+  proxychains4 nc -zv -w 2 $1 {} 2>&1 | grep -i "open" 
   sleep $(( RANDOM % 5 + 1)) # pausa aleatória de 1 a 5 segundos
 ' bash "$1"
 # bash $1 no final do comando é usado para passar o argumento $1
