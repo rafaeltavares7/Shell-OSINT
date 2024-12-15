42b3b840e3749400dac0622661cb496f041bbead
Rafael <168600337+rafaelcyber1@users.noreply.github.com>
2024-12-15 12:36:29 -0300

    Update enumdir.sh

--- enumdir.sh
+++ enumdir.sh
@@ -11,7 +11,7 @@ cat $wordlist | xargs -P $paralelo -I {} bash -c '
 # --spider faz com que o wget apenas faça uma "varredura" para verificar diretórios.
 # --timeout= define o tempo máximo total de espera
    wget --timeout=3 --tries=1 --spider --limit-rate=1K $1{} 2>&1 | grep "following"
-   sleep $(( RANDOM % 5 + 1)) # pausa aleatoria de 1 a 5 segundos
+   sleep $(( RANDOM % 3 + 1)) # pausa aleatoria de 1 a 5 segundos
 ' bash "$1"
 # bash $1 no final do comando é usado para passar o argumento $1
 # bash -c '...': Para executar os comandos dentro de um subshell.
