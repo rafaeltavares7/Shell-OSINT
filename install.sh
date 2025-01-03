f40bfd4f8eb6b0ccf585f80646a2f05b9524aa7a
Rafael <168600337+rafaelcyber1@users.noreply.github.com>
2025-01-03 09:45:41 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,11 +9,14 @@ apt install proxychains4 -y
 apt install jq -y
 apt install aircrack-ng -y
 apt install gpg -y
+apt install netcat-traditional -y
+apt install sshpass -y
 apt install python3 -y
 apt install python3-venv -y
 python3 -m venv yt-dlp-env
 source yt-dlp-env/bin/activate
 pip install yt-dlp
 pip install googlesearch-python
+
 echo "Tudo Pronto ;-)"
 echo -e "Desative o ambiente virtual com o seguinte comando: deactivate \n"
