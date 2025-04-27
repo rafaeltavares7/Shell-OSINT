cb1fd66988301000f40f9f8565933a4076e0c3f9
Rafael <168600337+rafaeltavares7@users.noreply.github.com>
2025-04-27 11:15:30 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-echo "Instalando..."
+echo -e "\nInstalando..."
 apt update -y
 apt upgrade -y
 apt install jq -y
-apt install proxychains -y
-apt install tor -y
-
-echo "Tudo Pronto ;-)"
+apt install ffmpeg -y
+apt install python3 -y
+apt install python3-venv -y
+python3 -m venv venv
+source venv/bin/activate
+pip install yt-dlp
+deactivate
+echo -e "\nTudo Pronto ;-)\n"
